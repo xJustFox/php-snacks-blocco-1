5ef68fc1251e0f3104779b5467afa4fb75824186
xJustFox <dodydaversa@gmail.com>
2024-01-24 14:42:18 +0100

    [Slack 1- COMPLETED] Added foreach and string concatenation

--- snack_1/slack_1.php
+++ snack_1/slack_1.php
@@ -32,3 +32,21 @@
         ],
     ];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <?php foreach($games as $game) { ?>
+            <li>
+                <?php echo $game['Home'].' - '.$game['Guest'].' | '.$game['Home_points'].' - '.$game['Guest_points'] ?>
+            </li>
+        <?php } ?>
+    </ul>
+</body>
+</html>
