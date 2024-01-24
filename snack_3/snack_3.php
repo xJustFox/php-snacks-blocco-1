1c17c2b1c87e89e5841804e0fd795767f00c9695
xJustFox <dodydaversa@gmail.com>
2024-01-24 16:22:54 +0100

    [Snack 3] Exercise completed

--- snack_3/snack_3.php
+++ snack_3/snack_3.php
@@ -1,6 +1,6 @@
 <?php
 
-$posts = [
+$articles = [
 
     '16/09/2023' => [
         [
@@ -40,3 +40,33 @@ $posts = [
     ],
 ];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <h1 class="text-center pt-4">ARTICLES</h1>
+    <div class="container-xl">
+
+        <?php foreach ($articles as $key => $article) { ?>
+            <div class="row flex-wrap py-4 gap-3 ">
+
+                <?php foreach($article as $elem) { ?>
+                    <div class="col border border-1 border-black p-4 ">
+                        <div class="col-12"><?php echo $key ?></div>
+                        <h4 class="mb-3"><?php echo $elem['title'] ?></h4>
+                        <p><?php echo $elem['text'] ?></p>
+                        <div class="text-end"><?php echo $elem['author'] ?></div>
+                    </div>
+                <?php } ?>
+            </div>
+            <hr class=" text-black ">
+        <?php } ?>
+    </div>
+</body>
+</html>
