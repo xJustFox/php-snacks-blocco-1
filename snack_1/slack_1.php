ad7f558ee209d5ecf33a244e9467662a1f21ca4a
xJustFox <dodydaversa@gmail.com>
2024-01-24 14:32:00 +0100

    [Slack 1] Created array

---
+++ snack_1/slack_1.php
@@ -0,0 +1,34 @@
+<?php 
+    $games = [
+        [
+            'Home' => 'Milano',
+            'Guest' => 'Parma',
+            'Home_points' => '57',
+            'Guest_points' => '35',
+        ],
+        [
+            'Home' => 'Padova',
+            'Guest' => 'Genova',
+            'Home_points' => '70',
+            'Guest_points' => '47',
+        ],
+        [
+            'Home' => 'Taranto',
+            'Guest' => 'Firenze',
+            'Home_points' => '25',
+            'Guest_points' => '39',
+        ],
+        [
+            'Home' => 'Taranto',
+            'Guest' => 'Firenze',
+            'Home_points' => '25',
+            'Guest_points' => '39',
+        ],
+        [
+            'Home' => 'Lecce',
+            'Guest' => 'Palermo',
+            'Home_points' => '80',
+            'Guest_points' => '13',
+        ],
+    ];
+?>
